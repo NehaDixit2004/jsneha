ed0e3b38d520ceae88f4e24bd95872631b007820
nehadixit2004 <nd2991939@gmail.com>
2024-06-08 11:53:28 +0530

    first trial

---
+++ index.js
@@ -0,0 +1,115 @@
+// var num =10;
+
+// {
+//     num =20
+// }
+// console.log("var",num)
+
+// // //let
+// // let num1 =10
+// // {
+// //     num1 = 20
+// // }
+// // console.log("let",num1)
+
+// // //const
+// // const num2=10
+// // {
+// //     num2 =20
+// // }
+// // console.log("const",num2)
+// // //
+
+
+// var num =10
+
+// {
+//     var num =20
+//     num =30
+//     console.log(num)
+// }
+// console.log(num)
+
+// let num1 =10
+// {
+//     let num1 =20
+//     num1 =30
+//     console.log(num1)
+// }
+// console.log(num1)
+// const num2 =10
+// {
+//     const num2 = 20
+//     num2 = 30
+//     console.log(num2)
+// }
+// console.log(num2);
+
+
+// let num1 = 10;
+// let num2 =20;
+
+// let sum = num1+ num2;
+// console.log(sum)
+// let sub = num1 - num2;
+// console.log(sub)
+// let multiply = num1*num2;
+// console.log(multiply)
+// let mod = num1%num2;
+// console.log(mod)
+// let divide = num1/num2;
+// console.log(divide)
+
+// let num1 = 21;
+// let num2 = 20;
+
+// let sum = ++num1
+// console.log(sum)
+
+// let num1= 21;
+// let num2 =10;
+//    let sum = num1++
+// console.log(num1)
+
+// let num1= 21;
+// let num2 =10;
+// let sum = --num1 + ++num2 + num1-- + num2++;
+// console.log(sum)
+// console.log(num1)
+// console.log(num2)
+
+// let checkIn = true
+// let checkOut = "10";
+// let obj ={}
+// let arr;
+// console.log(typeof arr)
+// console.log(typeof obj)
+// console.log(typeof checkIn)
+// console.log(typeof checkOut)
+// console.log(checkIn == checkOut);
+
+
+// let a = 10;
+// let b =20;
+// let c = 30;
+// let num1 =20
+// let num2 =30;
+// console.log(num1< num2);
+// let result = (a<b && b<c)?"a":(b<c)?"b":'c';
+// console.log(result);
+
+// let age = 23
+// let result =(age >18)?"yes":'no'
+// console.log(result);
+
+// let num1 =0;
+// let num2 = 0;
+
+// let result =(num1==num2)?"yes":"no";
+// console.log(result);
+
+
+let num1 = 10;
+let num2 = 20;
+let result=(typeof num1  > typeof num2);
+console.log(result);
